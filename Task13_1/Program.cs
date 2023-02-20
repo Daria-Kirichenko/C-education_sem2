1d26d0a0d3fdf80675a88c296d19c5044c3c223e
Daria <darakirichenko@gmail.com>
2023-02-20 21:55:35 +0300

    Task13 is done

---
+++ Task13_1/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadNumber("Введите число:");
+if(number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while(number > 999)
+    {
+        number = number / 10;
+    }
+}
+Console.WriteLine(number % 10);
